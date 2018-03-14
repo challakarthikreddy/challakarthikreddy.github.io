3e45e7353fbb38636989bb758b0fe51faae2848a
challakarthikredy <karthik@mobisir.net>
2018-03-14 12:30:23 +0530

    removed commented

--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,3 @@
-﻿/**
- *
- * Responsive website using AngularJS
- * http://www.script-tutorials.com/responsive-website-using-angularjs/
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Script Tutorials
- * http://www.script-tutorials.com/
- */
 
 'use strict';
 
